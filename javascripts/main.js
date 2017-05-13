bdbbc44cb97ee8f003209fa0ab6b523a50bf3209
Anessa Ortner <anessa.ortner@gmail.com>
2017-05-13 10:57:33 -0500

    firebase setup initialized

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
-var app = angular.module("redneckToDoApp", []); //"name of your app", [parameter allows for plug-ins];
+ //"name of your app", [parameter allows for plug-ins];
 
 //$scope connects your javascript to your DOM
 app.controller("NavCtrl", ($scope) => {
@@ -9,26 +9,7 @@ app.controller("NavCtrl", ($scope) => {
 app.controller("ItemCtrl", ($scope) => {
 	$scope.dog = "Woof";
 	$scope.showListView = true;
-	$scope.items = [
-        {
-          id: 0,
-          task: "mow the lawn",
-          isCompleted: true,
-          assignedTo: "Callan",
-        },
-        {
-          id: 1,
-          task: "grade quizzes",
-          isCompleted: false,
-          assignedTo: "Lauren",
-        },
-        {
-          id: 2,
-          task: "take a nap",
-          isCompleted: false,
-          assignedTo: "Zoe",
-        }
-      ];
+  $scope.items = [];
 
 	$scope.newItem = () => {
 		$scope.showListView = false;
