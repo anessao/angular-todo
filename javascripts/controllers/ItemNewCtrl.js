441c00ed49016f6a7e6550e1557239f08bef4a30
Anessa Ortner <anessa.ortner@gmail.com>
2017-05-15 21:33:15 -0500

    setting up factory and new firebase interactions

--- javascripts/controllers/ItemNewCtrl.js
+++ javascripts/controllers/ItemNewCtrl.js
@@ -1,20 +1,10 @@
-app.controller("ItemNewCtrl", function($http, $q, $scope, FIREBASE_CONFIG) {
-    let postNewItem = (newItem) => {
-    return $q((resolve, reject) => {
-      $http.post(`${FIREBASE_CONFIG.databaseURL}/items.json`, JSON.stringify(newItem))
-      .then((results) => {
-        resolve(results);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-    });
-  };
+app.controller("ItemNewCtrl", function($scope, ItemFactory) {
+    
   $scope.addNewItem = () => {
     $scope.newTask.isCompleted = false;
-    postNewItem($scope.newTask).then(() => {
+    ItemFactory.postNewItem($scope.newTask).then(() => {
       $scope.newTask = {};
-      
+
       //**************
       //switch views
       //**************
